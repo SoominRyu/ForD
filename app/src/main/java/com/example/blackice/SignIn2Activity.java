0e4a33723572d2d59e0aa508719e39154aa61f59
yehyun0704 <yehyun0704@gmail.com>
2021-07-10 00:34:52 +0900

    21.07.09

--- app/src/main/java/com/example/blackice/SignIn2Activity.java
+++ app/src/main/java/com/example/blackice/SignIn2Activity.java
@@ -2,18 +2,13 @@ package com.example.blackice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignIn2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-
+        setContentView(R.layout.activity_sign_in2);
     }
 }
---
+++ app/src/main/java/com/example/blackice/SignIn3Activity.java
@@ -0,0 +1,25 @@
+package com.example.blackice;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignIn3Activity extends AppCompatActivity {
+    //private Button goLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in3);
+
+        //LoginActivity로 연결
+        Button goLogin = findViewById(R.id.LoginBtn3);
+        goLogin.setOnClickListener(view -> {
+            Intent intent = new Intent(SignIn3Activity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+    }
+}
