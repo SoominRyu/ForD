397202ef94cf3f6ae46a0207eab1c2bef23f5f15
82ryusm <82ryusm@naver.com>
2021-07-10 23:41:43 +0900

    20210710 카카오맵 연동 성공

--- app/src/main/java/com/example/blackice/MainActivity.java
+++ app/src/main/java/com/example/blackice/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.blackice;
 
-import android.os.Bundle;
-import android.view.ViewGroup;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.ViewGroup;
+
 import net.daum.mf.map.api.MapView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
