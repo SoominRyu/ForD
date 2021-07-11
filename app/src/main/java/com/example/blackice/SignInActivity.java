b115241578f8ab302bdc6187a8f76907e9d67f80
he <kimhe045@gmail.com>
2021-07-11 22:47:03 +0900

    first commit

---
+++ app/src/main/java/com/example/blackice/SignInActivity.java
@@ -0,0 +1,20 @@
+package com.example.blackice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CompoundButton;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+
+
+    }
+
+}
