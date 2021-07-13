b75e93a3116341458f98c2b86fb4a68c8894672f
he <kimhe045@gmail.com>
2021-07-13 23:54:06 +0900

    회원가입(중복 ID/비밀번호 일치여부)

--- app/src/main/java/com/example/blackice/SignIn2Activity.java
+++ app/src/main/java/com/example/blackice/SignIn2Activity.java
@@ -1,5 +1,8 @@
 package com.example.blackice;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,8 +10,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
