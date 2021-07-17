0b2de2667bfb17a4da2b6e1b64596478daaead7d
yehyun0704 <yehyun0704@gmail.com>
2021-07-18 00:09:13 +0900

    21.07.18

--- app/src/main/java/com/example/blackice/MainActivity.java
+++ app/src/main/java/com/example/blackice/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.blackice;
 
+import android.os.Bundle;
+import android.view.ViewGroup;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import net.daum.mf.map.api.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
     }
+
+
+
+
 }
