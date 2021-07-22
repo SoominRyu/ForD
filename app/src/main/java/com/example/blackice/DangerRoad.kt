99c7ca2709c711b82902d008b2297d31c90f77fd
82ryusm <82ryusm@naver.com>
2021-07-23 01:14:03 +0900

    0723 위험 도로 신고 완성, 카카오맵 외부로 연결까지 함

--- app/src/main/java/com/example/blackice/DangerRoad.kt
+++ app/src/main/java/com/example/blackice/DangerRoad.kt
@@ -1,29 +1,63 @@
 package com.example.blackice
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.appcompat.widget.Toolbar
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_danger_road.*
+import android.widget.*
 
 
 class DangerRoad : AppCompatActivity() {
+    //카카오맵 패키지
+    val packagename_kakaomap = "net.daum.android.map"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_danger_road)
 
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolbar)
-        val ab = supportActionBar!!
-        ab.setDisplayHomeAsUpEnabled(true)
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        when (id) {
-            android.R.id.home -> {
-                finish()
-                return true
+
+        kakaoMap.setOnClickListener{
+            try{
+                val intent = packageManager.getLaunchIntentForPackage(packagename_kakaomap)
+                intent!!.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+
+            }catch(e:Exception){
+                val url = "market://details?id=$packagename_kakaomap"
+                val i = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(i)
+            }
+
+        }
+
+        // access the items of the list
+        val danger = resources.getStringArray(R.array.Danger)
+
+        val spinner = findViewById<Spinner>(R.id.spinner_DR)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                    android.R.layout.simple_spinner_item, danger)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+
+                    Toast.makeText(this@DangerRoad,
+                            getString(R.string.selected_item) + " " +
+                                    "" + danger[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
             }
         }
-        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/blackice/SignInActivity.java
+++ app/src/main/java/com/example/blackice/SignInActivity.java
@@ -1,15 +1,14 @@
 package com.example.blackice;
 
-
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.Toast;
 import android.widget.CompoundButton;
-
+import android.widget.Toast;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -47,12 +46,6 @@ public class SignInActivity extends AppCompatActivity {
             }
         });
 
-
-        button_ok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SignInActivity.this, SignIn2Activity.class);
-
         button_cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,13 +53,6 @@ public class SignInActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
-        button_cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(checkBox_all.isChecked() == true || checkBox1.isChecked() ==true&&checkBox2.isChecked() ==true&&checkBox3.isChecked() ==true) {
-                    Intent intent = new Intent(SignInActivity.this, LoginActivity.class);
 
         button_ok.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,8 +70,4 @@ public class SignInActivity extends AppCompatActivity {
 
     }
 
-
 }
-
-
-
