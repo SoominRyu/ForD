677c9c0845ed3bda1de01bd244a5deb5eccd2f91
jiwonjssk <jiwonjssk@naver.com>
2021-08-02 22:14:14 +0900

    setting complete

---
+++ app/src/main/java/com/example/blackice/Setting2Activity.java
@@ -0,0 +1,36 @@
+package com.example.blackice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class Setting2Activity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms);
+
+        Button back = (Button) findViewById(R.id.back);
+        Button next = (Button) findViewById(R.id.next);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent settingTerm = new Intent(getApplicationContext(), Setting1Activity.class);
+                startActivity(settingTerm);
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent settingTerm = new Intent(getApplicationContext(), Setting3Activity.class);
+                startActivity(settingTerm);
+            }
+        });
+    }
+}
